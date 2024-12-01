6223e50d535f50e3eb26d82b09dc68c5ea5c9a9f
Chaz Bailey <chaz.bailey@forgerock.com>
2024-12-01 14:05:13 +0000

    Refactor framework, making it much simpler

--- src/main/java/Day1.java
+++ src/main/java/Day1.java
@@ -1,64 +1,85 @@
+import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
 
-public class Day1 {
-
-    public static class Part1 extends Part {
-        int solve(List<String> inputArray) {
-            var leftNumbers = new ArrayList<Integer>();
-            var rightNumbers = new ArrayList<Integer>();
-            for (var line : inputArray) {
-                if (line.isEmpty()) {
-                    continue;
-                }
-                var numbers = line.split(" ");
-                leftNumbers.add(Integer.parseInt(numbers[0]));
-                rightNumbers.add(Integer.parseInt(numbers[numbers.length-1]));
-            }
-
-            var sortedLeft = leftNumbers.stream().sorted().toList();
-            var sortedRight = rightNumbers.stream().sorted().toList();
-
-            var sum = 0;
-            for (var i = 0; i < sortedLeft.size(); i++) {
-
-                var left = sortedLeft.get(i);
-                var right = sortedRight.get(i);
-                var diff = Math.abs(left - right);
-                sum += diff;
-            }
-
-            return sum;
+public class Day1 extends ProblemBase {
+    @Override
+    public Long solvePart1(List<String> inputArray) {
+        var leftNumbers = new ArrayList<Integer>();
+        var rightNumbers = new ArrayList<Integer>();
+        for (var line : inputArray) {
+            var numbers = line.split(" ");
+            leftNumbers.add(Integer.parseInt(numbers[0]));
+            rightNumbers.add(Integer.parseInt(numbers[numbers.length - 1]));
         }
+
+        var sortedLeft = leftNumbers.stream().sorted().toList();
+        var sortedRight = rightNumbers.stream().sorted().toList();
+
+        var sum = 0L;
+        for (var i = 0; i < sortedLeft.size(); i++) {
+            var left = sortedLeft.get(i);
+            var right = sortedRight.get(i);
+            var diff = Math.abs(left - right);
+            sum += diff;
+        }
+
+        return sum;
     }
 
-    public static class Part2 extends Part {
-        int solve(List<String> inputArray) {
-            var leftNumbers = new ArrayList<Integer>();
-            var rightMapOfFrequency = new HashMap<Integer, Integer>();
-            for (var line : inputArray) {
-                if (line.isEmpty()) {
-                    continue;
-                }
-                var numbers = line.split(" ");
-                leftNumbers.add(Integer.parseInt(numbers[0]));
-                rightMapOfFrequency.put(Integer.parseInt(numbers[numbers.length-1]),
-                        rightMapOfFrequency.getOrDefault(Integer.parseInt(numbers[numbers.length-1]), 0) + 1);
-            }
-
-            var sum = 0;
-            for (var i = 0; i < leftNumbers.size(); i++) {
-                var left = leftNumbers.get(i);
-                var right = rightMapOfFrequency.getOrDefault(left, 0);
-                if (right == 0) {
-                    continue;
-                }
-                var diff = Math.abs(left * right);
-                sum += diff;
-            }
-
-            return sum;
+    @Override
+    public Long solvePart2(List<String> inputArray) {
+        var leftNumbers = new ArrayList<Integer>();
+        var rightMapOfFrequency = new HashMap<Integer, Integer>();
+        for (var line : inputArray) {
+            var numbers = line.split(" ");
+            leftNumbers.add(Integer.parseInt(numbers[0]));
+            rightMapOfFrequency.put(Integer.parseInt(numbers[numbers.length - 1]),
+                    rightMapOfFrequency.getOrDefault(Integer.parseInt(numbers[numbers.length - 1]), 0) + 1);
         }
+
+        var sum = 0L;
+        for (var i = 0; i < leftNumbers.size(); i++) {
+            var left = leftNumbers.get(i);
+            var right = rightMapOfFrequency.getOrDefault(left, 0);
+            if (right == 0) {
+                continue;
+            }
+            var diff = Math.abs(left * right);
+            sum += diff;
+        }
+
+        return sum;
+    }
+
+    @Override
+    public Stream<Map.Entry<String, Long>> getPart1Examples() {
+        var input = """
+                3   4
+                4   3
+                2   5
+                1   3
+                3   9
+                3   3
+                """;
+        var expected = 11L;
+        return Stream.of(new AbstractMap.SimpleEntry<>(input, expected));
+    }
+
+    @Override
+    public Stream<Map.Entry<String, Long>> getPart2Examples() {
+        String input = """
+                3   4
+                4   3
+                2   5
+                1   3
+                3   9
+                3   3
+                """;
+        var expected = 31L;
+        return Stream.of(new AbstractMap.SimpleEntry<>(input, expected));
     }
 }
---
+++ src/main/java/Day2.java
@@ -0,0 +1,25 @@
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+public class Day2 extends ProblemBase {
+    @Override
+    public Long solvePart1(List<String> inputArray) {
+        return 0L;
+    }
+
+    @Override
+    public Long solvePart2(List<String> inputArray) {
+        return 0L;
+    }
+
+    @Override
+    public Stream<Map.Entry<String, Long>> getPart1Examples() {
+        return Stream.empty();
+    }
+
+    @Override
+    public Stream<Map.Entry<String, Long>> getPart2Examples() {
+        return Stream.empty();
+    }
+}
