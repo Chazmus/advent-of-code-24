56a31fa66f3fcd41614483e06f3656fc099822f3
Chaz Bailey <chaz.bailey@forgerock.com>
2024-12-01 09:20:52 +0000

    Solve Day 1

--- src/main/java/Day1.java
+++ src/main/java/Day1.java
@@ -1,39 +1,64 @@
-/*
---- Day 1: Trebuchet?! ---
-Something is wrong with global snow production, and you've been selected to take a look. The Elves have even given
-you a map; on it, they've used stars to mark the top fifty locations that are likely to be having problems.
-
-You've been doing this long enough to know that to restore snow operations, you need to check all fifty stars by
-December 25th.
-
-Collect stars by solving puzzles. Two puzzles will be made available on each day in the Advent calendar; the second
-puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!
-
-You try to ask why they can't just use a weather machine ("not powerful enough") and where they're even sending you
-("the sky") and why your map looks mostly blank ("you sure ask a lot of questions") and hang on did you just say the
-sky ("of course, where do you think snow comes from") when you realize that the Elves are already loading you into a
-trebuchet ("please hold still, we need to strap you in").
-
-As they're making the final adjustments, they discover that their calibration document (your puzzle input) has been
-amended by a very young Elf who was apparently just excited to show off her art skills. Consequently, the Elves are
-having trouble reading the values on the document.
-
-The newly-improved calibration document consists of lines of text; each line originally contained a specific
-calibration value that the Elves now need to recover. On each line, the calibration value can be found by combining
-the first digit and the last digit (in that order) to form a single two-digit number.
-
-For example:
-
-1abc2
-pqr3stu8vwx
-a1b2c3d4e5f
-treb7uchet
-In this example, the calibration values of these four lines are 12, 38, 15, and 77. Adding these together produces 142.
-
-Consider your entire calibration document. What is the sum of all of the calibration values?
-
-
- */
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class Day1 {
+
+    public static class Part1 extends Part {
+        int solve(List<String> inputArray) {
+            var leftNumbers = new ArrayList<Integer>();
+            var rightNumbers = new ArrayList<Integer>();
+            for (var line : inputArray) {
+                if (line.isEmpty()) {
+                    continue;
+                }
+                var numbers = line.split(" ");
+                leftNumbers.add(Integer.parseInt(numbers[0]));
+                rightNumbers.add(Integer.parseInt(numbers[numbers.length-1]));
+            }
+
+            var sortedLeft = leftNumbers.stream().sorted().toList();
+            var sortedRight = rightNumbers.stream().sorted().toList();
+
+            var sum = 0;
+            for (var i = 0; i < sortedLeft.size(); i++) {
+
+                var left = sortedLeft.get(i);
+                var right = sortedRight.get(i);
+                var diff = Math.abs(left - right);
+                sum += diff;
+            }
+
+            return sum;
+        }
+    }
+
+    public static class Part2 extends Part {
+        int solve(List<String> inputArray) {
+            var leftNumbers = new ArrayList<Integer>();
+            var rightMapOfFrequency = new HashMap<Integer, Integer>();
+            for (var line : inputArray) {
+                if (line.isEmpty()) {
+                    continue;
+                }
+                var numbers = line.split(" ");
+                leftNumbers.add(Integer.parseInt(numbers[0]));
+                rightMapOfFrequency.put(Integer.parseInt(numbers[numbers.length-1]),
+                        rightMapOfFrequency.getOrDefault(Integer.parseInt(numbers[numbers.length-1]), 0) + 1);
+            }
+
+            var sum = 0;
+            for (var i = 0; i < leftNumbers.size(); i++) {
+                var left = leftNumbers.get(i);
+                var right = rightMapOfFrequency.getOrDefault(left, 0);
+                if (right == 0) {
+                    continue;
+                }
+                var diff = Math.abs(left * right);
+                sum += diff;
+            }
+
+            return sum;
+        }
+    }
 }
--- src/test/java/Day1Test.java
+++ src/test/java/Day1Test.java
@@ -1,29 +1,58 @@
-/*
- * Copyright 2024 ForgeRock AS. All Rights Reserved
- *
- * Use of this code requires a commercial software license with ForgeRock AS.
- * or with one of its affiliates. All use shall be exclusively subject
- * to such license between the licensee and ForgeRock AS.
- */
-
-import org.junit.jupiter.api.Test;
+import java.util.Map;
 
 public class Day1Test {
 
-    String example1Input = """
-            1abc2
-            pqr3stu8vwx
-            a1b2c3d4e5f
-            treb7uchet
-            """;
+    public static String INPUT_FILE_NAME = "Day1";
 
-    @Test
-    void example1() {
+    public static class Part1Test extends TestBase {
+        String example1Input = """
+                3   4
+                4   3
+                2   5
+                1   3
+                3   9
+                3   3
+                """;
 
+        @Override
+        protected Part getPart() {
+            return new Day1.Part1();
+        }
+
+        @Override
+        protected String getInputFileName() {
+            return INPUT_FILE_NAME;
+        }
+
+        @Override
+        protected Map<String, Integer> getExamples() {
+            return Map.of(example1Input, 11);
+        }
     }
 
-    void finalTest(){
+    public static class Part2Test extends TestBase {
+        String example1Input = """
+                3   4
+                4   3
+                2   5
+                1   3
+                3   9
+                3   3
+                """;
 
+        @Override
+        protected Part getPart() {
+            return new Day1.Part2();
+        }
+
+        @Override
+        protected Map<String, Integer> getExamples() {
+            return Map.of(example1Input, 31);
+        }
+
+        @Override
+        protected String getInputFileName() {
+            return INPUT_FILE_NAME;
+        }
     }
-
 }
