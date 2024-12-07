3a2f771c9f572e94c7f9aa44516bf3828a7c36ef
Chaz Bailey <chaz.bailey@forgerock.com>
2024-12-07 06:42:19 +0000

    Cleanup Day 7

--- src/main/java/problems/day7/Day7.java
+++ src/main/java/problems/day7/Day7.java
@@ -12,30 +12,40 @@ import problems.ProblemBase;
 
 public class Day7 extends ProblemBase {
 
-    enum Part1Operators {
+    enum Part1Operators implements BiFunction<Long, Long, Long> {
 
         ADD(Long::sum),
         MULTIPLY((a, b) -> a * b),
         ;
 
-        public BiFunction<Long, Long, Long> function;
+        private final BiFunction<Long, Long, Long> function;
 
         Part1Operators(BiFunction<Long, Long, Long> function) {
             this.function = function;
         }
+
+        @Override
+        public Long apply(Long aLong, Long aLong2) {
+            return function.apply(aLong, aLong2);
+        }
     }
 
-    enum Part2Operators {
+    enum Part2Operators implements BiFunction<Long, Long, Long> {
 
         ADD(Long::sum),
         MULTIPLY((a, b) -> a * b),
-        CONCAT((a, b) -> Long.valueOf(a + String.valueOf( b))),
+        CONCAT((a, b) -> Long.valueOf(a + String.valueOf(b))),
         ;
 
-        public BiFunction<Long, Long, Long> function;
+        private final BiFunction<Long, Long, Long> function;
 
         Part2Operators(BiFunction<Long, Long, Long> function) {
             this.function = function;
+        }
+
+        @Override
+        public Long apply(Long aLong, Long aLong2) {
+            return function.apply(aLong, aLong2);
         }
     }
 
@@ -46,8 +56,8 @@ public class Day7 extends ProblemBase {
             var parts = line.split(": ");
             var testValue = Long.valueOf(parts[0]);
             var equationNumbers = Arrays.stream(parts[1].split(" ")).map(Long::valueOf).toList();
-            var testCases = generateCombinations(equationNumbers.size() - 1);
-            Long result = getAnyResult(testValue, equationNumbers, testCases);
+            var testCases = generateCombinations(equationNumbers.size() - 1, Part1Operators.class);
+            long result = getAnyResult(testValue, equationNumbers, testCases);
             if (result != 0) {
                 total += result;
             }
@@ -55,67 +65,6 @@ public class Day7 extends ProblemBase {
 
         return total;
     }
-
-    private Long getAnyResult(Long testValue, List<Long> equationNumbers, List<List<Part1Operators>> testCases) {
-        for (var testCase : testCases) {
-            Long result = equationNumbers.get(0);
-            for (int i = 0; i < testCase.size(); i++) {
-                result = testCase.get(i).function.apply(result, equationNumbers.get(i + 1));
-            }
-
-            if (result.equals(testValue)) {
-                return result;
-            }
-        }
-        return 0L;
-    }
-    private Long getAnyResultPart2(Long testValue, List<Long> equationNumbers, List<List<Part2Operators>> testCases) {
-        for (var testCase : testCases) {
-            Long result = equationNumbers.get(0);
-            for (int i = 0; i < testCase.size(); i++) {
-                result = testCase.get(i).function.apply(result, equationNumbers.get(i + 1));
-            }
-
-            if (result.equals(testValue)) {
-                return result;
-            }
-        }
-        return 0L;
-    }
-
-
-    public static List<List<Part1Operators>> generateCombinations(int length) {
-        List<List<Part1Operators>> result = new ArrayList<>();
-        Part1Operators[] values = Part1Operators.values();
-        int totalCombinations = (int) Math.pow(values.length, length);
-        for (int i = 0; i < totalCombinations; i++) {
-            List<Part1Operators> combination = new ArrayList<>();
-            int temp = i;
-            for (int j = 0; j < length; j++) {
-                combination.add(values[temp % values.length]);
-                temp /= values.length;
-            }
-            result.add(combination);
-        }
-        return result;
-    }
-
-    public static List<List<Part2Operators>> generatePart2Combinations(int length) {
-        List<List<Part2Operators>> result = new ArrayList<>();
-        Part2Operators[] values = Part2Operators.values();
-        int totalCombinations = (int) Math.pow(values.length, length);
-        for (int i = 0; i < totalCombinations; i++) {
-            List<Part2Operators> combination = new ArrayList<>();
-            int temp = i;
-            for (int j = 0; j < length; j++) {
-                combination.add(values[temp % values.length]);
-                temp /= values.length;
-            }
-            result.add(combination);
-        }
-        return result;
-    }
-
 
     @Override
     public Long solvePart2(List<String> inputArray) {
@@ -124,14 +73,47 @@ public class Day7 extends ProblemBase {
             var parts = line.split(": ");
             var testValue = Long.valueOf(parts[0]);
             var equationNumbers = Arrays.stream(parts[1].split(" ")).map(Long::valueOf).toList();
-            var testCases = generatePart2Combinations(equationNumbers.size() - 1);
-            Long result = getAnyResultPart2(testValue, equationNumbers, testCases);
+            var testCases = generateCombinations(equationNumbers.size() - 1, Part2Operators.class);
+            long result = getAnyResult(testValue, equationNumbers, testCases);
             if (result != 0) {
                 total += result;
             }
         }
 
         return total;
+    }
+
+    private Long getAnyResult(Long testValue, List<Long> equationNumbers, List<List<BiFunction<Long, Long, Long>>> testCases) {
+        for (var testCase : testCases) {
+            Long result = equationNumbers.getFirst();
+            for (int i = 0; i < testCase.size(); i++) {
+                result = testCase.get(i).apply(result, equationNumbers.get(i + 1));
+            }
+
+            if (result.equals(testValue)) {
+                return result;
+            }
+        }
+        return 0L;
+    }
+
+
+    public static <E extends Enum<E> & BiFunction<Long, Long, Long>> List<List<BiFunction<Long, Long,
+            Long>>> generateCombinations(int length,
+            Class<E> operatorsEnum) {
+        List<List<BiFunction<Long, Long, Long>>> result = new ArrayList<>();
+        E[] values = operatorsEnum.getEnumConstants();
+        int totalCombinations = (int) Math.pow(values.length, length);
+        for (int i = 0; i < totalCombinations; i++) {
+            List<BiFunction<Long, Long, Long>> combination = new ArrayList<>();
+            int temp = i;
+            for (int j = 0; j < length; j++) {
+                combination.add(values[temp % values.length]);
+                temp /= values.length;
+            }
+            result.add(combination);
+        }
+        return result;
     }
 
     @Override
