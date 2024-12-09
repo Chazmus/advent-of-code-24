6b23c4c5eab025cb687e6fc95f6db33489d752ef
Chaz Bailey <chaz.bailey@forgerock.com>
2024-12-09 10:49:13 +0000

    Tried making better, still not working

--- src/main/java/problems/day9/Day9.java
+++ src/main/java/problems/day9/Day9.java
@@ -1,10 +1,3 @@
-/*
- * Copyright 2024 ForgeRock AS. All Rights Reserved
- *
- * Use of this code requires a commercial software license with ForgeRock AS.
- * or with one of its affiliates. All use shall be exclusively subject
- * to such license between the licensee and ForgeRock AS.
- */
 package problems.day9;
 
 import java.util.ArrayList;
