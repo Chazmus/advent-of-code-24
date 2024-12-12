35f09e4b409b098201710434d36441eb82686070
Chaz Bailey <chaz.bailey@forgerock.com>
2024-12-12 09:16:29 +0000

    Add day 12

---
+++ src/main/java/problems/day12/Day12.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2024 ForgeRock AS. All Rights Reserved
+ *
+ * Use of this code requires a commercial software license with ForgeRock AS.
+ * or with one of its affiliates. All use shall be exclusively subject
+ * to such license between the licensee and ForgeRock AS.
+ */
+package problems.day12;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.provider.Arguments;
+
+import problems.ProblemBase;
+
+public class Day12 extends ProblemBase {
+    @Override
+    public Long solvePart1(List<String> inputArray) {
+        return 0L;
+    }
+
+    @Override
+    public Long solvePart2(List<String> inputArray) {
+        return 0L;
+    }
+
+    @Override
+    public Stream<Arguments> getPart1Examples() {
+        return Stream.empty();
+    }
+
+    @Override
+    public Stream<Arguments> getPart2Examples() {
+        return Stream.empty();
+    }
+}
