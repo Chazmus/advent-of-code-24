414efb7fed83d9863f05a3c12699350efcd02f88
Chaz Bailey <chaz.bailey@forgerock.com>
2024-12-17 09:53:14 +0000

    Day 17 Part 1 done

--- src/main/java/problems/day17/Day17.java
+++ src/main/java/problems/day17/Day17.java
@@ -7,6 +7,7 @@
  */
 package problems.day17;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -17,7 +18,33 @@ import problems.ProblemBase;
 public class Day17 extends ProblemBase {
     @Override
     public Long solvePart1(List<String> inputArray) {
+        var compooter = getCompooter(inputArray);
+        compooter.run();
+        System.out.println(String.join(",", compooter.output.stream().map(String::valueOf).toList()));
         return 0L;
+    }
+
+    private Compooter getCompooter(List<String> inputArray) {
+        var computer = new Compooter();
+        for (var line : inputArray) {
+            if (line.startsWith("Register A: ")) {
+                computer.registerA = Integer.parseInt(line.split(": ")[1]);
+            }
+            if (line.startsWith("Register B: ")) {
+                computer.registerB = Integer.parseInt(line.split(": ")[1]);
+            }
+            if (line.startsWith("Register C: ")) {
+                computer.registerC = Integer.parseInt(line.split(": ")[1]);
+            }
+            if (line.startsWith("Program: ")) {
+                var program = line.split(": ")[1].split(",");
+                computer.program = new int[program.length];
+                for (var i = 0; i < program.length; i++) {
+                    computer.program[i] = Integer.parseInt(program[i]);
+                }
+            }
+        }
+        return computer;
     }
 
     @Override
@@ -25,9 +52,92 @@ public class Day17 extends ProblemBase {
         return 0L;
     }
 
+
+    class Compooter {
+
+        int registerA;
+        int registerB;
+        int registerC;
+        int[] program;
+        int instructionPointer;
+        List<Integer> output = new ArrayList<>();
+
+
+        int getCombo(int input) {
+            return switch (input) {
+                case 0, 1, 2, 3 -> input;
+                case 4 -> registerA;
+                case 5 -> registerB;
+                case 6 -> registerC;
+                default -> throw new RuntimeException("Invalid input");
+            };
+        }
+
+        public void run() {
+            while (instructionPointer < program.length) {
+                var opcode = Instruction.values()[program[instructionPointer]];
+                var operand = program[instructionPointer + 1];
+                switch (opcode) {
+                    case adv:
+                        var numerator = Double.valueOf(registerA);
+                        var denominator = Math.pow(2, getCombo(operand));
+                        registerA = (int) (numerator / denominator);
+                        instructionPointer += 2;
+                        break;
+                    case bxl:
+                        registerB = registerB ^ operand;
+                        instructionPointer += 2;
+                        break;
+                    case bst:
+                        registerB = getCombo(operand) % 8;
+                        instructionPointer += 2;
+                        break;
+                    case jnz:
+                        if (registerA != 0) {
+                            instructionPointer = operand;
+                        } else {
+                            instructionPointer += 2;
+                        }
+                        break;
+                    case bxc:
+                        registerB = registerB ^ registerC;
+                        instructionPointer += 2;
+                        break;
+                    case out:
+                        output.add(getCombo(operand) % 8);
+                        instructionPointer += 2;
+                        break;
+                    case bdv:
+                        var numerator2 = Double.valueOf(registerA);
+                        var denominator2 = Math.pow(2, getCombo(operand));
+                        registerB = (int) (numerator2 / denominator2);
+                        instructionPointer += 2;
+                        break;
+                    case cdv:
+                        var numerator3 = Double.valueOf(registerA);
+                        var denominator3 = Math.pow(2, getCombo(operand));
+                        registerC = (int) (numerator3 / denominator3);
+                        instructionPointer += 2;
+                        break;
+                }
+            }
+        }
+
+        enum Instruction {
+            adv, bxl, bst, jnz, bxc, out, bdv, cdv
+        }
+    }
+
     @Override
     public Stream<Arguments> getPart1Examples() {
-        return Stream.empty();
+        return Stream.of(
+                Arguments.of("""
+                        Register A: 729
+                        Register B: 0
+                        Register C: 0
+                        
+                        Program: 0,1,5,4,3,0""", 0L)
+        );
     }
 
     @Override
